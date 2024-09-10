28d05d5314b6b59444714da64870d0b3e54abff6
tnestico <123529071+tnestico@users.noreply.github.com>
2024-09-10 11:28:13 -0400

    Update PitchPlotFunctions.py

--- PitchPlotFunctions.py
+++ PitchPlotFunctions.py
@@ -234,7 +234,7 @@ class PitchPlotFunctions:
         try:
             team_id = data_team['teams'][0]['parentOrgId']
             print('WE MADE IT HERE',team_id)
-            print([dict(scraper.get_teams().select(['team_id', 'parent_org_abbreviation']).iter_rows()))
+            print(dict(scraper.get_teams().select(['team_id', 'parent_org_abbreviation']).iter_rows()))
             logo_url = self.team_logos()[dict(scraper.get_teams().select(['team_id', 'parent_org_abbreviation']).iter_rows())[team_id]]
 
         except KeyError:
