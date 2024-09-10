6ef41bef37dd79d1ad30bbd952e64da4c75055c1
tnestico <123529071+tnestico@users.noreply.github.com>
2024-09-10 18:18:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ st.markdown("""
 
 Big thanks to [Michael Rosen](https://twitter.com/bymichaelrosen) and [Jeremy Maschino](https://twitter.com/pitchprofiler) for inspiration for this project
 
-[Check Out Michael's Pitch Plotting App Here](https://pitchplotgenerator.streamlit.app/).
+[Check Out Michael's *Pitch Plotting App*](https://pitchplotgenerator.streamlit.app/).
 
 [Check Out Jeremy's Website *Pitch Profiler*](http://www.mlbpitchprofiler.com/).
 """
